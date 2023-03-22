c6ab6ae6685a0b0655305b18e2c28d31081dce37
raaz maholia <maholialekhraj46@gmail.com>
2023-03-22 05:37:09 +0530

    Fixed Template Title Tag

--- templates/home.php
+++ templates/home.php
@@ -6,7 +6,7 @@ if(!defined('SECURE')) exit('<h1>Access Denied</h1>');
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Welcome | MPM</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   </head>
 <body>
