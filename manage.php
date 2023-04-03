fe9d32272f01039c6256d25a7c6bb2726d013264
raaz maholia <maholialekhraj46@gmail.com>
2023-04-03 09:55:56 +0530

    Fixed Typos

--- manage.php
+++ manage.php
@@ -1,5 +1,11 @@
 <?php
-require_once 'mpm/core/command_line.php';
-use function  Mpm\Core\execute_from_command_line;
-execute_from_command_line($argv);
+if(php_sapi_name()=="cli") {
+  require_once "config/settings.php";
+  require_once "mpm/database/sql_reader.php";
+  require_once "mpm/database/database_handler.php";
+  require_once 'mpm/core/command_line.php';
+}
+  
+  use function  Mpm\Core\execute_from_command_line;
+  execute_from_command_line($argv);
 ?>
